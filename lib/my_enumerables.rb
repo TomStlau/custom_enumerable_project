e9065a4f29a2f7cf1a6fcb434dea30f9114699ee
TomStlau <tommy.stlau@gmail.com>
2023-11-27 15:29:01 -0500

    First commit. Implemented 4 methods.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,46 @@
 module Enumerable
   # Your code goes here
+
+  def my_each_with_index
+    length.times do |index|
+      yield(self[index], index)
+    end
+    self
+  end
+
+  def my_any?(arg = nil)
+    if block_given?
+      self.my_each do |item|
+        return true if yield(item)
+      end
+    else
+      self.my_each do |item|
+        return true if item
+      end
+    end
+    false
+  end
+
+  def my_each
+    for i in self
+      yield(i)
+    end
+    self
+  end
+
+  def my_all?(arg = nil)
+    if block_given?
+      self.my_each do |item|
+        return false unless yield(item)
+      end
+    else
+      self.my_each do |item|
+        return false unless item
+      end
+    end
+    true
+
+  end
 end
 
 # You will first have to define my_each
